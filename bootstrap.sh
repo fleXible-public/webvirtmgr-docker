3053b4fb7dedcd86a7c3005a28b8e1d007776a1a
Felix Franz <dev@felix.neobits.de>
2020-07-02 20:01:17 +0200

    feat: Update to Ubuntu 16.04 LTS

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash -x
+
+set -o errexit    # exit on error
+set -o pipefail   # exit on error in pipe
 
 if [ ! -f "/data/vm/webvirtmgr.sqlite3" ]; then
-
-chown webvirtmgr /var/run/libvirt/libvirt-sock
-/usr/bin/python /webvirtmgr/manage.py syncdb --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', '1234')" | /usr/bin/python /webvirtmgr/manage.py shell
+  chown webvirtmgr /var/run/libvirt/libvirt-sock
+  /usr/bin/python /webvirtmgr/manage.py syncdb --noinput
+  echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', '1234')" | /usr/bin/python /webvirtmgr/manage.py shell
 
 fi
 
+supervisorctl start webvirtmgr
+supervisorctl start webvirtmgr-console
--- local_settings.py
+++ local_settings.py
@@ -1,10 +1,11 @@
 import os
 
 DATABASES = {
+    'LOCAL_PATH': '/data/vm',
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': '/data/vm/webvirtmgr.sqlite3',
-    }
+    },
 }
 
 
